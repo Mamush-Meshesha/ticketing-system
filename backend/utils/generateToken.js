d78c644f8bb9391b62705348c9d6aa2a2973eccf
Mamush-Meshesha <mamgithub@gmail.com>
2025-03-09 08:28:17 +0300

    middleware accept cookkie instead of header

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,15 +4,13 @@ const generateToken = (res,id, role) => {
     expiresIn: '1d',
   });
 
-  // res.cookie("token", token, {
-  //   httpOnly: true, 
-  //   secure: process.env.NODE_ENV === "production",  
-  //   maxAge: 3600 * 1000,
-  //   sameSite: "None",
-  // });
+  res.cookie("token", token, {
+    httpOnly: true, 
+    secure: process.env.NODE_ENV === "production",  
+    maxAge: 3600 * 1000,
+    sameSite: "None",
+  });
   
-  
-
     return token;
 }
 
